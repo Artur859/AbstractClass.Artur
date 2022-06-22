d84b7e76b983c14487f96d92827c142933b5d827
Artur859 <107873018+Artur859@users.noreply.github.com>
2022-06-22 14:24:39 +0200

    Add project files.

---
+++ AbstractClass.Artur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractClass.Artur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
